89748dc33832b928859701ba8e665011dc0d7341
oSumAtrIX <johan.melkonyan1@web.de>
2019-06-10 19:56:49 +0200

    Add files via upload

---
+++ VoiceMessage.plugin.js
@@ -0,0 +1,59 @@
+//META{"name":"VoiceMessage","source":"https://github.com/oSumAtrIX/BetterDiscordPlugins","website":"https://github.com/oSumAtrIX"}*//
+class VoiceMessage {
+    getName() {
+        return "VoiceMessage";
+    }
+    getDescription() {
+        return "Send Voice Messages just like in WhatsApp";
+    }
+    getVersion() {
+        return "0.0.1";
+    }
+    getAuthor() {
+        return "oSumAtrIX";
+    }
+    load() {
+
+    }
+    start() { }
+
+    stop() {
+
+    }
+    onSwitch() {
+        const btn = document.createElement("button");
+        btn.style.background = "url(data:image/svg+xml;base64,PHN2ZyBmaWxsPSIjRkZGRkZGIiBoZWlnaHQ9IjI0IiB2aWV3Qm94PSIwIDAgMjQgMjQiIHdpZHRoPSIyNCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4gICAgPHBhdGggZD0iTTEyIDE0YzEuNjYgMCAyLjk5LTEuMzQgMi45OS0zTDE1IDVjMC0xLjY2LTEuMzQtMy0zLTNTOSAzLjM0IDkgNXY2YzAgMS42NiAxLjM0IDMgMyAzem01LjMtM2MwIDMtMi41NCA1LjEtNS4zIDUuMVM2LjcgMTQgNi43IDExSDVjMCAzLjQxIDIuNzIgNi4yMyA2IDYuNzJWMjFoMnYtMy4yOGMzLjI4LS40OCA2LTMuMyA2LTYuNzJoLTEuN3oiLz4gICAgPHBhdGggZD0iTTAgMGgyNHYyNEgweiIgZmlsbD0ibm9uZSIvPjwvc3ZnPg==)";
+        btn.style.backgroundRepeat = "no-repeat";
+        btn.style.backgroundPosition = "center";
+        btn.style.filter = "brightness(0.5)";
+        btn.className = "recordAudio";
+        if (navigator.mediaDevices) {
+            navigator.mediaDevices.getUserMedia({
+                    audio: true
+                })
+                .then(function(stream) {
+                    var mediaRecorder = new MediaRecorder(stream),
+                        chunks = [];
+                    btn.addEventListener("mousedown", function() {
+                        btn.style.filter = "brightness(1.5)";
+                        mediaRecorder.start();
+                    });
+                    btn.addEventListener("mouseup", function() {
+                        btn.style.filter = "brightness(0.5)";
+                        setTimeout(mediaRecorder.stop(), 200)
+                    });
+                    mediaRecorder.onstop = function(e) {
+                        BdApi.findModuleByProps("instantBatchUpload").instantBatchUpload(BdApi.findModuleByProps("getChannelId").getChannelId(), [new File(chunks, "Voice Message.mp3")]);
+                        chunks = [];
+                    }
+                    mediaRecorder.ondataavailable = function(e) {
+                        chunks.push(e.data);
+                    }
+                })
+        };
+        if (!document.getElementsByClassName(btn.className)[0]) {
+            document.querySelector(".buttons-205you").appendChild(btn);
+        }
+    }
+
+}
---
+++ YTSearch.plugin.js
@@ -0,0 +1,37 @@
+//META{"name":"YTSearch","source":"https://github.com/oSumAtrIX/BetterDiscordPlugins","website":"https://github.com/oSumAtrIX"}*//
+class YTSearch {
+
+    getName() {
+        return "YTSearch";
+    }
+    getDescription() {
+        return "Send Youtube Videos by pressing [SHIFT]+[ENTER] while using the following syntax: 'yt [searchterm]'";
+    }
+    getVersion() {
+        return "0.0.1";
+    }
+    getAuthor() {
+        return "oSumAtrIX";
+    }
+    
+    onSwitch(){
+    	var textArea =  document.getElementsByClassName("textArea-2Spzkt")[0];
+        textArea.addEventListener("keyup", function(event) {
+            event.preventDefault();  
+            var content = textArea.value;
+            if (event.keyCode === 13) {
+                if (content.substring(0, 3).includes("yt ")) {
+                    $.getJSON('https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=' + content.substring(3, content.length) + '&type=video&key=AIzaSyB8yFpnD-iQqwgjBvQHgmw0SJn-JXBF7u0', function(data) {
+                        BdApi.findModuleByProps("sendMessage").sendMessage(BdApi.findModuleByProps("getChannelId").getChannelId(), {
+                            content: "https://www.youtube.com/watch?v=" + data.items[0].id.videoId
+                        });
+                    });
+                }
+            }
+        });
+    }
+
+    start() {}
+    stop() {}
+
+}

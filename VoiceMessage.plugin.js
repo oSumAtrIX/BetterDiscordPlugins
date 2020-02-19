9b57c93e5ba33ba876c8a4370a48e9e2851b088a
oSumAtrIX <johan.melkonyan1@web.de>
2020-02-19 21:54:05 +0100

    Update VoiceMessage.plugin.js
    SVG invisible bug

--- VoiceMessage.plugin.js
+++ VoiceMessage.plugin.js
@@ -7,7 +7,7 @@ class VoiceMessage {
         return "Send Voice Messages just like in WhatsApp";
     }
     getVersion() {
-        return "0.0.1";
+        return "1.1";
     }
     getAuthor() {
         return "oSumAtrIX";
@@ -15,53 +15,65 @@ class VoiceMessage {
     load() {
 
     }
-    start() { }
+    start() {
+
+    }
 
     stop() {
-    	
+
     }
     onSwitch() {
-    	if (document.getElementsByClassName("ghost-button-wrapper").length == 0) {
-        var daButtons = document.getElementsByClassName("buttons-205you")[0], ghostButton = document.createElement("button"), btnInner = document.createElement("div"), btn = document.createElement("img");
-        ghostButton.setAttribute("type", "button");
-        ghostButton.setAttribute("class", "buttonWrapper-1ZmCpA da-buttonWrapper button-38aScr da-button lookBlank-3eh9lL da-lookBlank colorBrand-3pXr91 da-colorBrand grow-q77ONN da-grow normal ghost-button-wrapper");
-        btn.setAttribute("src", "https://discordapp.com/assets/4bc527c257233fc69b94342d77bcb9ee.svg");
-        btn.setAttribute("class", "icon-3D60ES da-icon");
-        btn.setAttribute("width", "22");
-        btn.setAttribute("height", "22");
-        btnInner.setAttribute("class", "contents-18-Yxp da-contents button-3AYNKb da-button button-2vd_v_ da-button ghost-button-inner");
-        btnInner.appendChild(btn);
-        ghostButton.appendChild(btnInner);
-        daButtons.insertBefore(ghostButton, daButtons.firstChild);
+        var buttonRow = document.getElementsByClassName("buttons-3JBrkn")[0],
+            audioButton = document.createElement("button"),
+            svgContainer = document.createElement("svg"),
+            svgContainerAttributes = {
+                "width": "22",
+                "height": "22",
+                "fill": "currentColor",
+                "class": "icon-GhnIRB icon-3D60ES",
+                "viewBox": "0 -4 24 24"
+            },
+            svgPath = document.createElement("path");
 
-        if (navigator.mediaDevices) {
-            navigator.mediaDevices.getUserMedia({
-                    audio: true
-                })
-                .then(function(stream) {
-                    var mediaRecorder = new MediaRecorder(stream),
-                        chunks = [];
-                    btn.addEventListener("mousedown", function() {
-                        btn.style.filter = "brightness(1.5)";
-                        mediaRecorder.start();
-                    });
-                    btn.addEventListener("mouseup", function() {
-                        btn.style.filter = "brightness(0.5)";
-                        setTimeout(()=>mediaRecorder.stop(), 200)
-                    });
-                    mediaRecorder.onstop = function(e) {
-                        BdApi.findModuleByProps("instantBatchUpload").instantBatchUpload(BdApi.findModuleByProps("getChannelId").getChannelId(), [new File(chunks, "Voice Message.mp3")]);
-                        chunks = [];
-                    }
-                    mediaRecorder.ondataavailable = function(e) {
-                        chunks.push(e.data);
-                    }
-                })
-        };
+        for (var key in svgContainerAttributes) {
+            svgContainer.setAttribute(key, svgContainerAttributes[key]);
         }
-        if (!document.getElementsByClassName(btn.className)[0]) {
-            document.querySelector(".buttons-205you").appendChild(btn);
-        }
+
+        svgPath.setAttribute("d", "M7,4.4408921e-16 C8.65685425,0 10,1.34314575 10,3 L10,9 C10,10.6568542 8.65685425,12 7,12 C5.34314575,12 4,10.6568542 4,9 L4,3 C4,1.34314575 5.34314575,4.4408921e-16 7,0 L7,4.4408921e-16 Z M14,9 C14,12.53 11.39,15.44 8,15.93 L8,19 L6,19 L6,15.93 C2.61,15.44 0,12.53 0,9 L2,9 C2,11.7614237 4.23857625,14 7,14 C9.76142375,14 12,11.7614237 12,9 L14,9 L14,9 Z");
+        
+        audioButton.setAttribute("type", "button");
+        audioButton.setAttribute("class", "lookBlank-");
+        audioButton.addEventListener("onmouseenter", function() {
+            svgContainer.style.filter = "brightness(0.2)";
+        });
+
+        svgContainer.appendChild(svgPath);
+        audioButton.appendChild(svgContainer);
+        buttonRow.insertBefore(audioButton, buttonRow.firstChild);
+
+        navigator.mediaDevices.getUserMedia({
+            audio: true
+        }).then(function(stream) {
+            var mediaRecorder = new MediaRecorder(stream),
+                chunks = [];
+            audioButton.addEventListener("click", function() {
+                if (mediaRecorder.state === 'inactive') {
+                    audioButton.setAttribute("fill", "red");
+                    svgContainer.style.filter = "brightness(1)";
+                    mediaRecorder.start();
+                } else {
+                    svgContainer.style.filter = "brightness(0.7)";
+                    setTimeout(() => mediaRecorder.stop(), 200)
+                }
+            });
+            mediaRecorder.onstop = function(e) {
+                BdApi.findModuleByProps("instantBatchUpload").instantBatchUpload(BdApi.findModuleByProps("getChannelId").getChannelId(), [new File(chunks, "VoiceMessage-" + new Date().toString().slice(4, 21).replace(" ", "-").replace(" ", "-").replace(" ", " ").replace(":", "-") + ".mp3")]);
+                chunks = [];
+            }
+            mediaRecorder.ondataavailable = function(e) {
+                chunks.push(e.data);
+            }
+        })
     }
 
 }

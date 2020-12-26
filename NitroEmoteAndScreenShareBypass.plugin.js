551ebaeabe9c6982955f413a3400793c970c341c
oSumAtrIX <johan.melkonyan1@web.de>
2020-12-26 07:46:37 +0100

    Fix description & remove depedency of zeres lib

--- NitroEmoteAndScreenShareBypass.plugin.js
+++ NitroEmoteAndScreenShareBypass.plugin.js
@@ -4,42 +4,35 @@ class NitroEmoteAndScreenShareBypass {
         return "NitroEmoteAndScreenShareBypass";
     }
     getDescription() {
-        return "Send Nitro emojies without Nitro (lame bypass) and enable high quality screen sharing";
+        return "Send nitro emotes (lame bypass) and enable high quality screen sharing without Nitro";
     }
     getVersion() {
-        return "3.1";
+        return "3.2";
     }
     getAuthor() {
         return "oSumAtrIX";
     }
-    load() {
 
-    }
     start() {
-        const mod = ZeresPluginLibrary.DiscordModules.UserStore;
-        const checkExist = setInterval(() => {
-            const cUser = mod.getCurrentUser();
-            if (cUser == undefined) return;
-            cUser.premiumType = 2;
-            clearInterval(checkExist);
-        }, 500);
-    }
-
-    stop() {
-
+        const checkExist = setInterval(() => {	
+    		for (const mdl of Object.values(webpackJsonp.push([[],{['']:(_,e,r)=>{e.cache=r.c}},[['']]]).cache)) {
+    		    const d = mdl.exports.default;
+    		    if (d && d['getCurrentUser']) d.getCurrentUser().premiumType = 2;
+    		}
+        	clearInterval(checkExist);
+    	}, 1000);
     }
 
     onSwitch() {
-        const useFileUpload = true;
+        const useFileUpload = false;
         const div = document.getElementsByClassName("name-3YKhmS")[0];
-        const serverName = div != undefined ? div.innerHTML : "noServer";
+	    const serverName = div != undefined ? div.innerHTML : "noServer";
         const btn = $(".buttonContainer-28fw2U")[1];
         if (btn != null)
             btn.onclick = () => {
                 const checkExist = setInterval(function() {
                     const scroller = $(".listItems-1uJgMC")[0];
                     if (scroller == null) return;
-
                     clearInterval(checkExist);
                     scroller.parentElement.onclick = (e) => {
                         const target = e.target;

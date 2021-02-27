2045ba01935e8b0a80676a34ee01bac7c504fd52
oSumAtrIX <johan.melkonyan1@web.de>
2021-02-27 07:28:14 +0100

    Added inline emotes support
    Everything is poc of course and it will append text at the beginning of text if text is present. Line splitting will be added in the next update to compensate image width limit

--- NitroEmoteAndScreenShareBypass.plugin.js
+++ NitroEmoteAndScreenShareBypass.plugin.js
@@ -7,28 +7,39 @@ class NitroEmoteAndScreenShareBypass {
         return "Send nitro emotes (lame bypass) and enable high quality screen sharing without Nitro";
     }
     getVersion() {
-        return "3.2";
+        return "3.3";
     }
     getAuthor() {
         return "oSumAtrIX";
     }
-    stop() {}
-	
+    stop() {
+
+    }
     start() {
-        const checkExist = setInterval(() => {	
-    		for (const mdl of Object.values(webpackJsonp.push([[],{['']:(_,e,r)=>{e.cache=r.c}},[['']]]).cache)) {
-    		    const d = mdl.exports.default;
-    		    if (d && d['getCurrentUser']) d.getCurrentUser().premiumType = 2;
-    		}
-        	clearInterval(checkExist);
-    	}, 1000);
+        const checkExist = setInterval(() => {
+            for (const mdl of Object.values(webpackJsonp.push([
+                    [], {
+                        ['']: (_, e, r) => {
+                            e.cache = r.c
+                        }
+                    },
+                    [
+                        ['']
+                    ]
+                ]).cache)) {
+                const d = mdl.exports.default;
+                if (d && d['getCurrentUser']) d.getCurrentUser().premiumType = 2;
+            }
+            clearInterval(checkExist);
+        }, 1000);
     }
 
     onSwitch() {
-        const useFileUpload = false;
-        const div = document.getElementsByClassName("name-3YKhmS")[0];
-	    const serverName = div != undefined ? div.innerHTML : "noServer";
-        const btn = $(".buttonContainer-28fw2U")[1];
+        const
+            useFileUpload = false,
+            div = document.getElementsByClassName("name-3YKhmS")[0],
+            serverName = div != undefined ? div.innerHTML : "noServer",
+            btn = $(".buttonContainer-28fw2U")[1];
         if (btn != null)
             btn.onclick = () => {
                 const checkExist = setInterval(function() {
@@ -36,22 +47,66 @@ class NitroEmoteAndScreenShareBypass {
                     if (scroller == null) return;
                     clearInterval(checkExist);
                     scroller.parentElement.onclick = (e) => {
-                        const target = e.target;
-                        const src = target.firstChild.src;
-			const ext = src.slice(-7, -4);
-			    
-                        if (ext == "gif" || target.parentElement.parentElement.children[0].firstChild.children[1].innerHTML != serverName) {
+                        const
+                            target = e.target,
+                            src = target.firstChild.src;
+                        if (src.slice(-7, -4) == "gif" || target.parentElement.parentElement.children[0].firstChild.children[1].innerHTML != serverName) {
                             const curChannel = BdApi.findModuleByProps("getLastSelectedChannelId").getChannelId();
-                            const url = src.slice(0, -4) + "?size=40";
-                            useFileUpload ?
-                                fetch(url)
-                                .then(res => res.blob()).then(blob => {
-                                    BdApi.findModuleByProps("instantBatchUpload").instantBatchUpload(curChannel, [new File([blob], 'oSumAtrIX.'+ext, blob)]);
-                                }) :
-                                BdApi.findModuleByProps("sendMessage").sendMessage(curChannel, {
-                                    content: url
+                            var url = src.slice(0, -4);
+                            var ext = url.slice(url.length - 3);
+
+                            function upLoad(blob) {
+                                BdApi.findModuleByProps("instantBatchUpload").instantBatchUpload(curChannel, [new File([blob], 'oSumAtrIX.' + ext, blob)]);
+                            }
+
+                            var txtBar = $(".textArea-12jD-V")[0];
+                            if (txtBar.children.length != 1) {
+                                url = url + "?size=40";
+                                useFileUpload ?
+                                    fetch(url)
+                                    .then(res => res.blob()).then(upLoad) :
+                                    BdApi.findModuleByProps("sendMessage").sendMessage(curChannel, {
+                                        content: url
+                                    });
+                                return;
+                            }
+
+                            var measuringSpan = document.createElement("span");
+                            measuringSpan.innerText = txtBar.textContent.split(' ').join('M');
+                            measuringSpan.style.display = 'none';
+                            document.body.appendChild(measuringSpan);
+                            var width = $(measuringSpan).width();
+
+                            var img = document.createElement("img");
+                            img.crossOrigin = "Anonymous"
+                            document.body.appendChild(img);
+
+                            var img = document.createElement("img");
+                            img.crossOrigin = "Anonymous"
+                            document.body.appendChild(img);
+                            img.onload = (me) => {
+                                var img = $(me.path[0]);
+                                var canvas = document.createElement('canvas'),
+                                    ctx = canvas.getContext('2d');
+                                canvas.width = img.width() + width + 10;
+                                canvas.crossOrigin = "Anonymous";
+                                canvas.height = img.height();
+                                ctx.drawImage(img.get(0), 0, 0);
+                                ctx.font = "16px Whitney";
+                                ctx.clearRect(0, 0, canvas.width, canvas.height);
+                                ctx.drawImage(img.get(0), 0, 0);
+                                ctx.fillStyle = "#DCDDDE";
+                                ctx.fillText(txtBar.textContent, 27, 15);
+                                canvas.toBlob((blob) => {
+                                    upLoad(blob);
+                                    img.remove();
+                                    measuringSpan.remove();
                                 });
+                            };
+
+                            img.src = url + "?size=20";
                         }
+
                     }
                 }, 100);
             }

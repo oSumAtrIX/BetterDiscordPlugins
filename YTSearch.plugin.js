8f7380a812a6fcf15649a9cc4fde9ffed2537d37
oSumAtrIX <johan.melkonyan1@web.de>
2021-12-03 14:00:54 +0100

    Fix undefined module and add update url to meta

--- YTSearch.plugin.js
+++ YTSearch.plugin.js
@@ -1,40 +1,50 @@
-//META{"name":"YTSearch","source":"https://github.com/oSumAtrIX/BetterDiscordPlugins","website":"https://github.com/oSumAtrIX"}*//
+/**
+ * @name YTSearch
+ * @author oSumAtrIX
+ * @authorId 737323631117598811
+ * @version 2.4.2
+ * @description Send videos from YouTube.
+ * @website https://osumatrix.me
+ * @source https://github.com/oSumAtrIX/BetterDiscordPlugins
+ * @updateUrl https://raw.githubusercontent.com/oSumAtrIX/BetterDiscordPlugins/master/YTSearch.plugin.js
+ */
 class YTSearch {
     getName() {
         return "YTSearch";
     }
     getDescription() {
-        return "Send Youtube Videos by pressing [SHIFT]+[ENTER] while using the following syntax: 'yt [searchterm]'";
+        return "Send videos from YouTube. Usage: yt [searchterm] (SHIFT+ENTER to send the youtube video)";
     }
     getVersion() {
-        return "2.3";
+        return "2.4.2";
     }
     getAuthor() {
         return "oSumAtrIX";
     }
-    getKey() {return BdApi.getData('YTSearch', 'apiKey')}
+    getKey() {
+        return BdApi.getData('YTSearch', 'apiKey')
+    }
     getSettingsPanel() {
-    	const key = this.getKey();
+        const key = this.getKey();
         return `<input type="password" class="inputDefault-_djjkz input-cIJ7To" name="apiKey" type=text value="` + (key == undefined ? "" : key) + `" placeholder="YouTube Data API v3 Key" onChange="key = this.value; BdApi.setData('YTSearch', 'apiKey', this.value)">
 <br><a target="_blank" href="https://console.developers.google.com/apis/library/youtube.googleapis.com?q=youtube">Get it here</a>
         `;
     }
     onSwitch() {
-        const doc = document.getElementsByClassName("textArea-12jD-V")[0], key = this.getKey();
-        if (doc == undefined) return;
-        doc.addEventListener('keyup', (e) => {
-        	const text = doc.textContent;
-            if (e.keyCode === 13 && text.substring(0, 3).includes('yt ')) {
-                $.getJSON('https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=' + encodeURIComponent(text.substring(3, text.length)) + '&type=video&key=' + key, function(data) {
-                    BdApi.findModuleByProps("sendMessage").sendMessage(BdApi.findModuleByProps("getChannelId").getChannelId(), {
-                        content: 'https://www.youtube.com/watch?v=' + data.items[0].id.videoId
+        const tb = document.getElementsByClassName("textArea-12jD-V")[0],
+            key = this.getKey();
+        if (tb == undefined) return;
+        tb.addEventListener('keyup', (e) => {
+            const content = tb.textContent;
+            if (e.keyCode === 13 && content.substring(0, 3).includes('yt ')) {
+                fetch('https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=' + content + '&type=video&key=' + this.getKey()).then(r => r.json()).then(r => {
+                    BdApi.findModuleByProps("sendMessage").sendMessage(BdApi.findModuleByProps("getLastSelectedChannelId").getChannelId(), {
+                        content: 'https://www.youtube.com/watch?v=' + r.items[0].id.videoId
                     });
-                });
+                })
             }
         });
     }
-
     start() {}
     stop() {}
-
 }

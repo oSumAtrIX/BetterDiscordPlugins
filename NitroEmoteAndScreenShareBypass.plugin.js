87f6658abfcc953e6433da43ac7aa07ba32d8127
oSumAtrIX <johan.melkonyan1@web.de>
2022-01-13 17:31:04 +0100

    Fix url slicing and selectors

--- NitroEmoteAndScreenShareBypass.plugin.js
+++ NitroEmoteAndScreenShareBypass.plugin.js
@@ -2,7 +2,7 @@
  * @name NitroEmoteAndScreenShareBypass
  * @author oSumAtrIX
  * @authorId 737323631117598811
- * @version 3.6.1
+ * @version 3.6.3
  * @description Send Nitro emotes and enable high quality screen sharing without Nitro
  * @website https://osumatrix.me
  * @source https://github.com/oSumAtrIX/BetterDiscordPlugins
@@ -16,7 +16,7 @@ class NitroEmoteAndScreenShareBypass {
 		return "Send Nitro emotes and enable high quality screen sharing without Nitro";
 	}
 	getVersion() {
-		return "3.6.2";
+		return "3.6.3";
 	}
 	getAuthor() {
 		return "oSumAtrIX";
@@ -41,15 +41,15 @@ class NitroEmoteAndScreenShareBypass {
 	onSwitch() {
 		const
 			useFileUpload = false,
-			div = document.getElementsByClassName("name-3YKhmS")[0],
+			div = document.getElementsByClassName("name-3Uvkvr")[0],
 			serverName = div != undefined ? div.innerHTML : "noServer",
-			btnContainer = document.getElementsByClassName("buttons-3JBrkn")[0].children,
+			btnContainer = document.getElementsByClassName("buttons-uaqb-5")[0].children,
 			btn = btnContainer[btnContainer.length - 1];
 
 		if (btn != null)
 			btn.onclick = () => {
 				const checkExist = setInterval(function() {
-					const scroller = document.getElementsByClassName("listItems-1uJgMC")[0];
+					const scroller = document.getElementsByClassName("listItems-6eZzQ1")[0];
 					if (scroller == null) return;
 					clearInterval(checkExist);
 					scroller.parentElement.onclick = (e) => {
@@ -57,17 +57,16 @@ class NitroEmoteAndScreenShareBypass {
 							target = e.target,
 							src = target.firstChild.src;
 						if (src == undefined) return;
-						let server = target.parentElement.parentElement.children[0].firstChild.children[1] || document.querySelector("div.colorStandard-2KCXvj.size12-3cLvbJ.titleSecondary-3Dh_RZ").children[0];
-						if (src.slice(-7, -4) == "gif" || server != serverName) {
-							const curChannel = BdApi.findModuleByProps("getLastSelectedChannelId").getChannelId();
-							var url = src.slice(0, -2);
-							var ext = url.slice(url.length - 9, -6);
 
+						let server = target.parentElement.parentElement.children[0].firstChild.children[1] || document.querySelector("div.colorStandard-21JIj7.size12-oc4dx4.titleSecondary-3hcpuB").children[0];
+						let url = src.slice(0, -19);
+						let ext = url.slice(url.length - 9, -6);
+						if (ext == "gif" || server != serverName) {
+							const curChannel = BdApi.findModuleByProps("getLastSelectedChannelId").getChannelId();
 							function upload(blob) {
 								BdApi.findModuleByProps("instantBatchUpload").instantBatchUpload(curChannel, [new File([blob], 'oSumAtrIX.' + ext, blob)]);
 							}
 
-							var txtBar = document.getElementsByClassName("textArea-12jD-V")[0];
 							url = url + "40";
 							useFileUpload ?
 								fetch(url)

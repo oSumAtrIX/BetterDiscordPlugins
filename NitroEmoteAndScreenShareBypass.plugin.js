2c08fb615991135b0737ddb2cc5c4087582f95a8
oSumAtrIX <johan.melkonyan1@web.de>
2022-02-05 09:02:04 +0100

    Update nitro plugin reaching stable release

--- NitroEmoteAndScreenShareBypass.plugin.js
+++ NitroEmoteAndScreenShareBypass.plugin.js
@@ -2,83 +2,151 @@
  * @name NitroEmoteAndScreenShareBypass
  * @author oSumAtrIX
  * @authorId 737323631117598811
- * @version 3.6.4
+ * @version 4.0
  * @description Send Nitro emotes and enable high quality screen sharing without Nitro
  * @website https://osumatrix.me
  * @source https://github.com/oSumAtrIX/BetterDiscordPlugins
  * @updateUrl https://raw.githubusercontent.com/oSumAtrIX/BetterDiscordPlugins/master/NitroEmoteAndScreenShareBypass.plugin.js
  */
-class NitroEmoteAndScreenShareBypass {
-	getName() {
-		return "NitroEmoteAndScreenShareBypass";
-	}
-	getDescription() {
-		return "Send Nitro emotes and enable high quality screen sharing without Nitro";
-	}
-	getVersion() {
-		return "3.6.4";
-	}
-	getAuthor() {
-		return "oSumAtrIX";
-	}
-	stop() {
+module.exports = (() => {
+  const config = {
+    info: {
+      name: "NitroEmoteAndScreenShareBypass",
+      authors: [
+        {
+          name: "oSumAtrIX",
+          discord_id: "737323631117598811",
+          github_username: "oSumAtrIX",
+        },
+      ],
+      version: "4.0",
+      description:
+        "Send Nitro emotes and enable high quality screen sharing without Nitro",
+      github: "https://github.com/oSumAtrIX/BetterDiscordPlugins",
+      github_raw:
+        "https://raw.githubusercontent.com/oSumAtrIX/BetterDiscordPlugins/master/NitroEmoteAndScreenShareBypass.plugin.js",
+    },
+    main: "NitroEmoteAndScreenShareBypass.plugin.js",
+  };
 
-	}
-	start() {
-		const mod = BdApi.findModuleByProps("getCurrentUser")
-		let tries;
-		const checkExist = setInterval(() => {
-			let cUser;
-			if ((cUser = mod.getCurrentUser()) != undefined) {
-				cUser.premiumType = 2;
-				tries = 10;
-			}
-			if (++tries > 10)
-				clearInterval(checkExist);
-		}, 1000);
-	}
+  return !global.ZeresPluginLibrary
+    ? class {
+        constructor() {
+          this._config = config;
+        }
+        getName() {
+          return config.info.name;
+        }
+        getAuthor() {
+          return config.info.authors.map((a) => a.name).join(", ");
+        }
+        getDescription() {
+          return config.info.description;
+        }
+        getVersion() {
+          return config.info.version;
+        }
+        load() {
+          BdApi.showConfirmationModal(
+            "Library Missing",
+            `The library plugin needed for ${config.info.name} is missing. Please click Download Now to install it.`,
+            {
+              confirmText: "Download Now",
+              cancelText: "Cancel",
+              onConfirm: () => {
+                require("request").get(
+                  "https://rauenzi.github.io/BDPluginLibrary/release/0PluginLibrary.plugin.js",
+                  async (error, response, body) => {
+                    if (error)
+                      return require("electron").shell.openExternal(
+                        "https://betterdiscord.net/ghdl?url=https://raw.githubusercontent.com/rauenzi/BDPluginLibrary/master/release/0PluginLibrary.plugin.js"
+                      );
+                    await new Promise((r) =>
+                      require("fs").writeFile(
+                        require("path").join(
+                          BdApi.Plugins.folder,
+                          "0PluginLibrary.plugin.js"
+                        ),
+                        body,
+                        r
+                      )
+                    );
+                  }
+                );
+              },
+            }
+          );
+        }
+        start() {}
+        stop() {}
+      }
+    : (([Plugin, Api]) => {
+        const plugin = (Plugin, Api) => {
+          const { Patcher, DiscordModules, Settings, PluginUtilities } = Api;
+          return class NitroEmoteAndScreenShareBypass extends Plugin {
+            settings = PluginUtilities.loadSettings(this.getName(), {
+              size: 48,
+            });
+            getSettingsPanel() {
+              return Settings.SettingPanel.build(
+                (_) =>
+                  PluginUtilities.saveSettings(this.getName(), this.settings),
+                ...[
+                  new Settings.Slider(
+                    "Size",
+                    "The size of the emotes.",
+                    16,
+                    64,
+                    this.settings.size,
+                    (size) => (this.settings.size = size),
+                    {
+                      markers: [16, 20, 32, 48, 64, 128],
+                      stickToMarkers: true,
+                    }
+                  ),
+                ]
+              );
+            }
 
-	onSwitch() {
-		const
-			useFileUpload = false,
-			div = document.getElementsByClassName("name-3Uvkvr")[0],
-			serverName = div != undefined ? div.innerHTML : "noServer",
-			btnContainer = document.getElementsByClassName("buttons-uaqb-5")[0].children,
-			btn = btnContainer[btnContainer.length - 1];
+            onStart() {
+              ZeresPluginLibrary.DiscordModules.UserStore.getCurrentUser().premiumType = 2;
 
-		if (btn != null)
-			btn.onclick = () => {
-				const checkExist = setInterval(function() {
-					const scroller = document.getElementsByClassName("listItems-6eZzQ1")[0];
-					if (scroller == null) return;
-					clearInterval(checkExist);
-					scroller.parentElement.onclick = (e) => {
-						const
-							target = e.target,
-							src = target.firstChild.src;
-						if (src == undefined) return;
+              Patcher.before(
+                DiscordModules.MessageActions,
+                "sendMessage",
+                (_, [, message]) => {
+                  const emojies = message.validNonShortcutEmojis;
 
-						let server = target.parentElement.parentElement.children[0].firstChild.children[1] || document.querySelector("div.colorStandard-21JIj7.size12-oc4dx4.titleSecondary-3hcpuB").children[0];
-						let url = src.slice(0, -19);
-						let ext = url.split(/[#?]/)[0].split('.').pop().trim();
-						if (ext == "gif" || server != serverName) {
-							const curChannel = BdApi.findModuleByProps("getLastSelectedChannelId").getChannelId();
-							function upload(blob) {
-								BdApi.findModuleByProps("instantBatchUpload").instantBatchUpload(curChannel, [new File([blob], 'oSumAtrIX.' + ext, blob)]);
-							}
+                  emojies.forEach((emoji) => {
+                    // skip discord emojies
+                    if (!emoji.require_colons) return;
 
-							url = url + "40";
-							useFileUpload ?
-								fetch(url)
-								.then(res => res.blob()).then(upload) :
-								BdApi.findModuleByProps("sendMessage").sendMessage(curChannel, {
-									content: url
-								});
-						}
+                    // create the emojie string which we will replace
+                    const emoteString = `<${emoji.animated ? "a" : ""}:${
+                      emoji.name
+                    }:${emoji.id}>`;
 
-					}
-				}, 100);
+                    let url = emoji.url;
 
-			}
-	}
-}
+                    // change the size of the emojie in the url
+                    const size = this.settings.size;
+                    if (size != 48) {
+                      url = url.replace(/\?size=[0-9]+/, `?size=${size}`);
+                    }
+
+                    // replace the message containing the emojie with the url
+                    message.content = message.content.replace(emoteString, url);
+                  });
+                }
+              );
+            }
+
+            onStop() {
+              Patcher.unpatchAll();
+            }
+          };
+        };
+        return plugin(Plugin, Api);
+      })(global.ZeresPluginLibrary.buildPlugin(config));
+})();
+/*@end@*/
